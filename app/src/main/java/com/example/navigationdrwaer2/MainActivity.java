22ce5e591e9da00c8ddc4ca59f82e7b8c78b5c79
NaimAsSakib <rifat.sakib90@gmail.com>
2022-01-10 18:52:56 +0600

    Designed layout inside Navigation view instead of taking item. It's easy to design as I wish. set onclickListener on textviews inside NavigationView

--- app/src/main/java/com/example/navigationdrwaer2/MainActivity.java
+++ app/src/main/java/com/example/navigationdrwaer2/MainActivity.java
@@ -7,31 +7,78 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
+    private TextView tvMessage, tvArchivedMessage, tvNotification, tvProfile, tvChangePass, tvLogout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        tvMessage = findViewById(R.id.tvMessage);
+        tvArchivedMessage = findViewById(R.id.tvArchivedMessage);
+        tvNotification = findViewById(R.id.tvNotification);
+        tvProfile = findViewById(R.id.tvProfile);
+        tvChangePass = findViewById(R.id.tvChangePass);
+        tvLogout = findViewById(R.id.tvLogout);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawerLayout=findViewById(R.id.drawerLayout);
+        drawerLayout = findViewById(R.id.drawerLayout);
 
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        tvMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new MessageFragment()).commit();
+                Toast.makeText(getApplicationContext(), "Message panel is open", Toast.LENGTH_LONG).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
+        tvArchivedMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new ArchivedMessageFragment()).commit();
+                Toast.makeText(getApplicationContext(), "Archived Message panel is open", Toast.LENGTH_LONG).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
+        tvNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new NotificationFragment()).commit();
+                Toast.makeText(getApplicationContext(), "Notification panel is open", Toast.LENGTH_LONG).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
+
+
+
     }
 
     @Override
     public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
     }
